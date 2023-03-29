4102f137b798ae9d4ab3cc5fb054846521b60a3e
Jason Allan <jsallan@gmail.com>
2023-03-28 23:24:42 -0700

    new object factories to support MCU_cutter and Keyboard objects

--- Modules/object_factories.py
+++ Modules/object_factories.py
@@ -4,3 +4,23 @@ def keyhole_factory(component, key, x, y, z, rot):
     new_key = class_defs.Keyhole(component, f"row{key}", x, y, z, rot)
     new_key.create_keyhole()
     return new_key
+
+def box_factory(component, name, height, width, depth, rot, x_center=0.0, y_center=0.0, z_center=0.0):
+    new_box = class_defs.Box(component, height, width, depth, name, x_center=x_center, y_center=y_center, z_center=z_center)
+    new_box.create()
+    return new_box
+
+def matrix_factory(parent_comp, name):
+    new_matrix = class_defs.Matrix(parent_comp, name)
+    new_matrix.create()
+    return new_matrix
+
+def thumbs_factory(parent_comp, name, col, place_func):
+    new_thumbs = class_defs.Thumbs(parent_comp, name, col=0, placement_function=place_func)
+    new_thumbs.create()
+    return new_thumbs
+
+def mcu_cutter_factory(parent_comp, name, tl_x, tl_y, tl_z=0):
+    new_mcu_cutter = class_defs.MCU_cutter(parent_comp, name, tl_x=tl_x, tl_y=tl_y, tl_z=tl_z)
+    new_mcu_cutter.create()
+    return new_mcu_cutter
